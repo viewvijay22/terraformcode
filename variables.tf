6d31b79c62b9489640e2fcf1aa3d2374aa9a7b91
vjdevopsaws99 <vjdevosp99@gmai.com>
2026-05-12 00:40:09 +0530

    Change instance type to t3.micro

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "ami" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "instance_name" {
