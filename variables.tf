040e6fb49d2c683a7ccd111311528b2ea574cef4
vjdevopsaws99 <vjdevosp99@gmai.com>
2026-05-12 00:34:35 +0530

    Update AMI ID to ami-000dd7709fc500032

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "ami" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c55b159cbfafe1d0"  # Amazon Linux 2 in us-east-1
+  default     = "ami-000dd7709fc500032"  # Updated AMI ID
 }
 
 variable "instance_type" {
